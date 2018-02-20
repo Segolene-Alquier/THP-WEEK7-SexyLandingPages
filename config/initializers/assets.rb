b620d4244b4999119cc32283767a3a58e5530bfa
Segolene Alquier <segolene.alquier@yahoo.com>
2018-02-20 23:24:41 +0100

    checkr fixed

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,5 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( checkr.css )
-Rails.application.config.assets.paths << "#{Rails.root}/app/assets/media"
 Rails.application.config.assets.precompile += %w( sparks.css )
